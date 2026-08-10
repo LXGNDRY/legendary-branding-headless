1eac5c6921555b79c48889ee21d3bc0ba2673cfa
Legendary Branding <legendarybranding25@gmail.com>
2026-08-09 21:29:12 -0500

    Modify environment variables in env.d.ts
    Updated environment variable definitions with new values.

--- env.d.ts
+++ env.d.ts
@@ -6,16 +6,16 @@
  * All PUBLIC_ vars are safe to expose to the client.
  */
 interface Env {
-  SESSION_SECRET: string;
-  PUBLIC_STOREFRONT_API_TOKEN: string;
-  PRIVATE_STOREFRONT_API_TOKEN: string;
-  PUBLIC_STORE_DOMAIN: string;
-  PUBLIC_STOREFRONT_ID: string;
+  SESSION_SECRET: string;8e96826de52a65ce266649a1f3c99c7d1b2e3096
+  PUBLIC_STOREFRONT_API_TOKEN: string;76e8d43b034761793649bfaf68a1903c
+  PRIVATE_STOREFRONT_API_TOKEN: string;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
+  PUBLIC_STORE_DOMAIN: string;lngndny.myshopify.com
+  PUBLIC_STOREFRONT_ID: string;1000167667
   PUBLIC_STOREFRONT_API_VERSION: string;
-  PUBLIC_CUSTOMER_ACCOUNT_API_CLIENT_ID: string;
-  PUBLIC_CUSTOMER_ACCOUNT_API_URL: string;
-  PUBLIC_CHECKOUT_DOMAIN: string;
-  SHOP_ID: string;
+  PUBLIC_CUSTOMER_ACCOUNT_API_CLIENT_ID: string;d0428666-bac9-4bea-9d8a-5ece5001e1a8
+  PUBLIC_CUSTOMER_ACCOUNT_API_URL: string;https://shopify.com/49013915801
+  PUBLIC_CHECKOUT_DOMAIN: string;legendary-branding.com
+  SHOP_ID: string;49013915801
 }
 
 declare module 'virtual:react-router/server-build' {
